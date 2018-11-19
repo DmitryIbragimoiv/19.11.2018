540d0f8a9dd7b6cbb3a95f2ac3862c2e5dac7bd7
DmitryIbragimoiv <43035500+DmitryIbragimoiv@users.noreply.github.com>
2018-11-19 20:42:29 +0300

    11
    11

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php 
+
+$arr = array(41,22,46,7,0,-2,-3,41,0,-17,-1);
+$max = $arr[0];
+for($t=0;$t<=;$t++){
+	if($max<$arr[$t]){
+		$max = $arr[$t];
+	};
+};
+echo "max" .$max;
+
+?>
+
+<?php 
+echo "<br/>";
+$arr = array(41,22,46,7,0,-2,-3,41,0,-17,-1);
+
+for($i = 0, $sum = 0; $i <10; $i++)
+	if($arr[$i] > 0)
+		$sum += $arr[$i];
+
+echo $sum;
+?>
